1ac3f1f28aab1ee2ccef6aaf721f233aea27003d
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-14 20:30:59 -0300

    objects in JS

---
+++ immersion2/day6/object-example.js
@@ -0,0 +1,29 @@
+// Objeto
+var House = {
+    size: 50,
+    color: "green",
+    rooms: 3,
+}
+
+console.log(House);
+
+
+House.size = 80;
+
+var House2 = {};
+House2.size = 39;
+House2.name = "japones";
+
+
+// Pseudo-class
+var House = function (size, color, rooms) {
+    this.size = size;
+    this.color = color;
+    this.rooms = rooms;
+};
+
+
+var house1 = new House(50, "green", 3);
+var house2 = new House(200, "white", 10);
+
+console.log(house1);
