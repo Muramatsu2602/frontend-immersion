31e4875b5e2d9a261de08296a82495ea134503d6
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-21 20:56:14 -0300

    using functions in App object

--- immersion2/day7/challenge/main.js
+++ immersion2/day7/challenge/main.js
@@ -18,7 +18,6 @@ container.style.backgroundColor = "blue";
 container.style.fontFamily = "arial";
 
 container.style.justifyContent = "space-between";
-
 container.style.flexDirection = "column";
 
 document.body.appendChild(container);
@@ -59,34 +58,29 @@ header_logo.appendChild(header_logo_content);
 
 // Body
 var body = document.createElement("div");
-var body_video_container = document.createElement("div");
-var body_video_content = document.createElement("video");
+var body_content = document.createElement("div");
 
-body_video_container.style.height = "30vh";
-body_video_container.style.width = "100vw";
-body_video_container.style.backgroundColor = "green";
-
+body_content.style.height = "30vh";
+body_content.style.width = "100vw";
+body_content.style.backgroundColor = "green";
 
 container.appendChild(body);
-body.appendChild(body_video_container);
+body.appendChild(body_content);
 
 // Footer
 var footer = document.createElement("div");
+var footer_content = document.createElement("div");
 
 footer.style.color = "white";
 footer.style.backgroundColor = "grey";
 footer.style.fontSize = "15px";
 footer.style.textAlign = "left";
-
 footer.style.position = "absolute";
 footer.style.bottom = "0";
 footer.style.width = "100vw";
 footer.style.height = "5vh";
 
-container.appendChild(footer);
-
-var footer_content = document.createElement("div");
-
 footer_content.innerHTML = "I am the Footer";
 
+container.appendChild(footer);
 footer.appendChild(footer_content);
