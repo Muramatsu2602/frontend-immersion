24553cdabf117a8531bcdefa414cf5df67313e10
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-28 20:58:31 -0300

    Lesson 10 completed

--- immersion2/day10/js.js
+++ immersion2/day10/js.js
@@ -7,14 +7,56 @@ var App = {
 
     console.log("Fim do App");
   },
-  store: {},
+  store: {
+    state: {
+      isDropdownOpen: false,
+    },
+    todos: [
+      {
+        userId: 1,
+        id: 1,
+        title: "hey my boss how are you",
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "hey my boss tjos os asdjfka;sfa",
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "hey my boss all gud",
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "hey my boss how are you doing tonight",
+        completed: false,
+      },
+    ],
+  },
   controllers: {
+    openDropdown: function () {
+      console.log("vamos abrir o dropdown", App.store.state.isDropdownOpen);
+      if (App.store.state.isDropdownOpen) {
+        console.log("esta fechado");
+        App.store.state.isDropdownOpen = false;
+        App.elements.dropDownContainer.style.display = "none";
+      } else {
+        console.log("Esta fechado, vamos abrir");
+        App.store.state.isDropdownOpen = true;
+      }
+    },
     createDiv: function () {
       var el = document.createElement("div");
       el.innerHTML = "Sou um div novo";
       el.style.border = "1px solid black";
 
-      el.setAttribute("i", App.elements.els.length);
+      // static id doesnt work -->
+      el.setAttribute(`${App.elements.els.length}`, App.elements.els.length);
 
       // deletes the generated i whenever we click on it
       el.onclick = function (event) {
@@ -26,12 +68,40 @@ var App = {
       App.elements.elsContainer.appendChild(el);
       App.elements.els.push(el);
     },
+    renderAllTodos: function () {
+      console.log("vamos iniciar e renderizar todos os todos");
+      var todos = App.store.state.todos;
+
+      for (let i = 0; i < todos.length.length; i++) {
+        const todo = todos[i].id + "-" + todo.title;
+        this.renderTodo(todo);
+      }
+    },
+    renderTodo: function (todo) {
+      console.log("vamos renderizr o:", todo);
+      var el = document.createElement("div");
+      var completed = "x";
+
+      if (todo.completed) {
+        completed = "CHECK!";
+      }
+
+      el.innerHTML = completed + "  -  " + todo.id + " - " + todo.title;
+
+      App.elements.todos(todo.id) = el;
+      App.elements.todosContainer.appendChild(el);
+      console.log(el);
+    },
   },
   elements: {
     app: null,
     header: null,
     body: null,
     footer: null,
+    button: null,
+    dropDownContainer: null,
+    todosContainer: null,
+    todos: {},
 
     createApp: function () {
       // container
@@ -58,6 +128,20 @@ var App = {
       this.body.innerHTML = "Eu sou body";
       this.body.style.border = " 1px solid green";
       this.app.appendChild(this.body);
+
+      this.button = document.createElement("button");
+      this.button.innerHTML = "click";
+      this.button.onclick = App.controllers.createDiv;
+      this.body.appendChild(this.button);
+
+      this.dropDownContainer = document.createElement("div");
+      this.dropDownContainer.style.border = "1px solid red";
+      this.dropDownContainer.style.width = "100px";
+      this.controllers.openDropdown();
+
+      this.postsContainer = document.createElement("div");
+      this.postsContainer.style.border = "1px solid green";
+      this.body.appendChild(this.postsContainer);
     },
 
     createFooter: function () {
